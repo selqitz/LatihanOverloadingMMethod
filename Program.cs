a8592b2c8edadbfba93e29ed9246ece0661d3c50
selqitz <101630390+selqitz@users.noreply.github.com>
2022-04-07 01:33:43 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using LatihanOverloadingMethod;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Numbers number = new Numbers();
+
+        Console.WriteLine("Minimum #1: {0}", number.FindMinimum(3, 1));
+        Console.WriteLine("Minimum #2: {0}", number.FindMinimum(3, 2, 4));
+        Console.WriteLine(); // tambahkan baris kosong
+        Console.WriteLine("Maximum #1: {0}", number.FindMaximum(3, 1));
+        Console.WriteLine("Maximum #2: {0}", number.FindMaximum(3, 2, 4));
+
+        Console.ReadKey();
+    }
+}
